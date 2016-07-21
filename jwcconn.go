815113a97e682f0961de794f0e03ee4fa0ea6c58
scnace <scbizu@gmail.com>
2016-07-21 23:02:35 +0800

    need OCR verify code....

--- jwcconn.go
+++ jwcconn.go
@@ -12,6 +12,7 @@ import (
 	"io/ioutil"
 	"net/http/cookiejar"
 
+	"github.com/scbizu/Zafu_jwcInterface/jwc_api/jwcpkg"
 	"github.com/scbizu/mahonia"
 )
 
@@ -23,19 +24,21 @@ var VIEWSTATE string = ""
 
 const (
 	//用户名
-	username string = "YourStudentNo"
+	username string = "201305070123"
 	//密码
-	password string = "Yourpassword"
+	password string = "jsm19950907!"
 	//模拟登陆第一个入口地址
 	login_url_gate0 string = "http://210.33.60.8/"
 	//模拟登陆第一个入口验证码地址
 	vrcode_url_gate0 string = "http://210.33.60.8/CheckCode.aspx"
 	//首页地址
-	logged_url string = "http://210.33.60.8/xs_main.aspx?xh=YourStudentNo"
+	logged_url string = "http://210.33.60.8/xs_main.aspx?xh=201305070123"
 	//默认登录页
 	default_url string = "http://210.33.60.8/default2.aspx"
 	//课程表
-	courseURL string = "http://210.33.60.8/xskbcx.aspx?xh=YourStudentNo"
+	courseURL string = "http://210.33.60.8/xskbcx.aspx?xh="
+
+	examURL string = "http://210.33.60.8/xskscx.aspx?xh="
 )
 
 func checkError(err error) {
@@ -69,27 +72,27 @@ func getsp(url string) map[string]string {
 /**
 *带cookie去拿第二波不明变量   否则 会出现Object Move <a href>here</a>
  */
-func getspAG(cookies []*http.Cookie, c *http.Client, name string, userno string) map[string]string {
-	req, _ := http.NewRequest("GET", "http://210.33.60.8/xskbcx.aspx?xh="+userno+"&xm="+url.QueryEscape(name)+"&gnmkdm=N121603", nil)
-	for _, v := range cookies {
-		req.AddCookie(v)
-	}
-	response, _ := c.Do(req)
-	//去拿__VIEWSTATE
-	body, err := ioutil.ReadAll(response.Body)
-	checkError(err)
-	regular := `<input.type="hidden".name="__VIEWSTATE".value="(.*)" />`
-	pattern := regexp.MustCompile(regular)
-	VIEWSTATE := pattern.FindAllStringSubmatch(string(body), -1)
-	//拿__VIEWSTATEGENERATOR
-	retor := `<input.type="hidden".name="__VIEWSTATEGENERATOR".value="(.*)" />`
-	patterntor := regexp.MustCompile(retor)
-	VIEWSTATEGENERATOR := patterntor.FindAllStringSubmatch(string(body), -1)
-	res := make(map[string]string)
-	res["VIEWSTATE"] = VIEWSTATE[0][1]
-	res["VIEWSTATEGENERATOR"] = VIEWSTATEGENERATOR[0][1]
-	return res
-}
+// func getspAG(cookies []*http.Cookie, c *http.Client, name string, userno string) map[string]string {
+// 	req, _ := http.NewRequest("GET", "http://210.33.60.8/xskbcx.aspx?xh="+userno+"&xm="+url.QueryEscape(name)+"&gnmkdm=N121603", nil)
+// 	for _, v := range cookies {
+// 		req.AddCookie(v)
+// 	}
+// 	response, _ := c.Do(req)
+// 	//去拿__VIEWSTATE
+// 	body, err := ioutil.ReadAll(response.Body)
+// 	checkError(err)
+// 	regular := `<input.type="hidden".name="__VIEWSTATE".value="(.*)" />`
+// 	pattern := regexp.MustCompile(regular)
+// 	VIEWSTATE := pattern.FindAllStringSubmatch(string(body), -1)
+// 	//拿__VIEWSTATEGENERATOR
+// 	retor := `<input.type="hidden".name="__VIEWSTATEGENERATOR".value="(.*)" />`
+// 	patterntor := regexp.MustCompile(retor)
+// 	VIEWSTATEGENERATOR := patterntor.FindAllStringSubmatch(string(body), -1)
+// 	res := make(map[string]string)
+// 	res["VIEWSTATE"] = VIEWSTATE[0][1]
+// 	res["VIEWSTATEGENERATOR"] = VIEWSTATEGENERATOR[0][1]
+// 	return res
+// }
 
 /**
 *模拟post表单
@@ -169,6 +172,20 @@ func getCourseData(c *http.Client) string {
 	return string(allData)
 }
 
+func GetExaminfo(c *http.Client) string {
+	ExamURL := examURL + username
+	req, err := http.NewRequest("GET", ExamURL, nil)
+	//NICE
+	req.Header.Set("Referer", ExamURL)
+	checkError(err)
+	finalRes, err := c.Do(req)
+	checkError(err)
+	allData, err := ioutil.ReadAll(finalRes.Body)
+	checkError(err)
+	finalRes.Body.Close()
+	return string(allData)
+}
+
 //MAIN
 
 func main() {
@@ -192,10 +209,10 @@ func main() {
 		//用刚才生成的cookie去爬 验证码   否则会504!!!!!
 
 		res, _ = c.Do(req)
-		file, _ := os.Create("verify.gif")
+		file, _ := os.Create("verifyccode.gif")
 		io.Copy(file, res.Body)
 
-		fmt.Println("请查看verify.gif， 然后输入验证码， 看不清输入0重新获取验证码")
+		fmt.Println("请查看verifyccode.gif， 然后输入验证码， 看不清输入0重新获取验证码")
 		fmt.Scanf("%s", &verify_code)
 		if verify_code != "0" {
 			break
@@ -203,18 +220,18 @@ func main() {
 		res.Body.Close()
 	}
 
-	cookies := post(default_url, c, username, password, verify_code, VIEWSTATE, VIEWSTATEGENERATOR, temp_cookies)
-	fmt.Println(temp_cookies)
-	fmt.Println(cookies)
-	//	fmt.Printf("%v\n",c.Jar)
-	//	for val,_:=range c.Jar.Cookies{
-	//		fmt.Println(val)
-	//		}
-	data := Testpage(c)
-	fmt.Println(data)
-	Name := getStuName(c)
-	fmt.Println(Name)
+	_ = post(default_url, c, username, password, verify_code, VIEWSTATE, VIEWSTATEGENERATOR, temp_cookies)
 
-	course := getCourseData(c)
-	fmt.Println(course)
+	// fmt.Println(temp_cookies)
+	// fmt.Println(cookies)
+	// data := Testpage(c)
+	// fmt.Println(data)
+	// Name := getStuName(c)
+	// fmt.Println(Name)
+	// course := getCourseData(c)
+	// fmt.Println(course)
+
+	exam := GetExaminfo(c)
+	examInfo := jwcpkg.FetchExam(exam)
+	fmt.Println(examInfo)
 }
